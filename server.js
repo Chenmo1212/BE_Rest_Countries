d15c0a25420602a5f1137e164039ff6f7fd1faee
chenmo <chenmo1212@qq.com>
2023-10-27 00:39:01 +0100

    feat: add fetch all countries api

--- server.js
+++ server.js
@@ -5,11 +5,23 @@ const port = 3001;
 
 app.use(express.json());
 
-// Define a route to fetch country information
-app.get('/country/:name', async (req, res) => {
+// Define a route to fetch all countries
+app.get('/all', async (req, res) => {
+    try {
+        const response = await axios.get('https://restcountries.com/v3.1/all');
+        const countries = response.data;
+        res.json(countries);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// Define a route to fetch country data by name
+app.get('/name/:name', async (req, res) => {
     try {
         const countryName = req.params.name;
-        const response = await axios.get(`https://restcountries.com/v3.1/name/${countryName}`);
+        const response = await axios.get(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
         const countryData = response.data[0]; // Assuming the first result is the correct country
 
         if (!countryData) {
